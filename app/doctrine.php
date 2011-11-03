49e7bfae5a875dde95e84be398d87d11355a2cbe
Oleg Zinchenko <olegz@default-value.com>
2011-11-03 02:52:42 +0200

    doctrine

--- app/doctrine.php
+++ app/doctrine.php
@@ -1,3 +1,13 @@
-php app/console doctrine:schema:create
+<?php
 
+function show_run($text, $command, $canFail = false)
+{
+    echo "\n* $text\n$command\n";
+    passthru($command, $return);
+    if (0 !== $return && !$canFail) {
+        echo "\n/!\\ The command returned $return\n";
+        exit(1);
+    }
+}
 
+show_run("doctrine", "app/console doctrine:schema:create");
